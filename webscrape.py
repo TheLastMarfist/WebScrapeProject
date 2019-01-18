f227474c8511967d83bf993dce907d9d2b15af55
Alexander Marx <43764994+TheLastMarfist@users.noreply.github.com>
2019-01-18 10:26:41 -0800

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -8,13 +8,16 @@ import time
 
 
 def main():
+	# user inputs
 	websiteurl = input("What is the website url of the website you want to track?")
 	numofminutes = int(input("After how many minutes do you check the website?"))
-	account_sid = "ACf7a274e7d148a2036820f83f753ef12a"
-	# Your Auth Token from twilio.com/console
-	auth_token  = "a82c62ca2316987c134dd91f8fc3eb3b"
+	account_sid = input("What is your Twilio account sid?")
+	auth_token  = input("What is you Twilio authentication token?")
+	twiliophonenumber = int(input("What is your Twilio phone number?"))
+	userphonenumber = int(input("What is you phone number?"))
+	# setting up messaging
 	client = Client(account_sid, auth_token)
-	#https://thelastmarfist.github.io/WebScrapeProject/
+	# scraping the website as it is when you run the script
 	page = requests.get(websiteurl)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	html = list(soup.children)[2]
@@ -24,21 +27,23 @@ def main():
 
 
 	while 1:
+		#checking for changes
 		page = requests.get(websiteurl)
 		soup = BeautifulSoup(page.content, 'html.parser')
 		html = list(soup.children)[2]
 		body = list(html.children)[1]
 		p = list(body.children)[3]
 		newtext = p.get_text()
+		#comparing if something has changed
 		if newtext != oldtext:
 			oldtext = newtext
 			message = client.messages.create(
-				to="+19712843287", 
-				from_="+13522898955",
+				to=userphonenumber, 
+				from_=twiliophonenumber,
 				body="Your website has changed.")
 		else:
 			print("nothing has changed")
-
+		#setiing up the automation loop
 		dt = datetime.now() + timedelta(minutes = numofminutes)
 		while datetime.now() < dt:
 			time.sleep(1)
